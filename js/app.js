290678c3005f7f26a9324994260f6098699624ad
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-18 18:33:10 +0200

    Added initApp()

--- js/app.js
+++ js/app.js
@@ -1,18 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sideNavIco = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sideNavIco, {});
-});
+'use strict'
 
-let base = []
-getFriends(5)
+const MAIN = document.querySelector('#main')
+
+const API_LINK = 'https://randomuser.me/api/'
+const APP_DELAY = 1500
+
+const BASE = []
+
+
+initApp()
+
+
+function initApp() {
+    const timeout = setTimeout(() => {
+        changeContent(createStartScreen())
+        clearTimeout(timeout)
+    }, APP_DELAY);
+    addListeners()
+}
+
+function addListeners() {
+    document.addEventListener('DOMContentLoaded', function() {
+        const sideNavIco = document.querySelectorAll('.sidenav')
+        M.Sidenav.init(sideNavIco, {})
+    })
+}
+
+function createStartScreen() {
+
+}
+
+function changeContent(content, callback) {
+    MAIN.classList.add('hide')
+    MAIN.addEventListener('transitionend', function() {
+        MAIN.innerHTML = ''
+        MAIN.innerHTML = content
+        callback && callback()
+        MAIN.classList.remove('hide')
+    }, { once: true })
+}
 
 function getFriends(num) {
-    fetch(`https://randomuser.me/api/?results=${num}`)
+    fetch(`${API_LINK}?results=${num}`)
         .then(function(resp) {
             return resp.json()
         }).then(function(data) {
             for (let index = 0; index < data.results.length; index++) {
-                base.push(data.results[index]);
+                BASE.push(data.results[index]);
             }
         })
         .catch(function() {
@@ -21,4 +55,4 @@ function getFriends(num) {
 }
 
 
-console.log(base);
+// console.log(BASE);
