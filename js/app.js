1842f85d437fad9bb7d492b6227dba404346a5ce
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-18 18:46:25 +0200

    Added music

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ const MAIN = document.querySelector('#main')
 const API_LINK = 'https://randomuser.me/api/'
 const APP_DELAY = 1500
 
+//Music
+const APP_AUDIO = new Audio('./audio/you_ve_got_a_friend_in_me.mp3')
+APP_AUDIO.loop = true
+APP_AUDIO.volume = 0.3
+
 const BASE = []
 
 
@@ -27,7 +32,8 @@ function addListeners() {
 }
 
 function createStartScreen() {
-
+    const greeting = `<h3 class="greeting">${GREETING_TEXT}</h3>`
+    return greeting
 }
 
 function changeContent(content, callback) {
