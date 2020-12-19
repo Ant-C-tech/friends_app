14b4a68d0b4fd36971688608cf215ba3f56f03c7
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-19 14:41:09 +0200

    Added getRandomIntInclusive changed main icon

--- js/app.js
+++ js/app.js
@@ -72,5 +72,11 @@ function getFriends(num) {
         })
 }
 
+function _getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 // console.log(BASE);
