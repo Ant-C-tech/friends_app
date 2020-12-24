63f0eb5143d9c3608f4fc0ea76d7861a5757582b
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-24 15:50:42 +0200

    Removed gramatic mistake with error message

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ function getFriends(num) {
         })
         .catch(function(error) {
             console.log(error.message)
-            changeContent(`<h1 class="container h-100 flexContainerCol errorMes">Something went wrong, we are so sorry: (Please, reload the page!</h1>`, SHOW_ELEM_PRIMARY_ANIMATION, HIDE_ELEM_PRIMARY_ANIMATION)
+            changeContent(`<h1 class="container h-100 flexContainerCol errorMes">Something went wrong, we are so sorry :( Please, reload the page!</h1>`, SHOW_ELEM_PRIMARY_ANIMATION, HIDE_ELEM_PRIMARY_ANIMATION)
         })
 }
 
